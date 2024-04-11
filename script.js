aef40074ea766ccd0e9529487d57e2fa1aebea73
Ansh Pandey <anshpandey8368@gmail.com>
2024-04-11 18:10:10 +0530

    change UI

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+document.addEventListener("DOMContentLoaded", function() {
+  var areas = document.querySelectorAll('area');
 
-for(var i=0; i<numberOfDrumButtons; i++){
-  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-    var buttonInnerHtml = this.innerHTML;
-    //For click 
-    createSound(buttonInnerHtml);//w,a,s,d,k,l
-    buttonAnimation(buttonInnerHtml);
-  })
-}
+  areas.forEach(function(area) {
+    area.addEventListener('click', function() {
+      var sound = new Audio(this.getAttribute('data-sound'));
+      sound.play();
+    });
+  });
 
-//for keyboard click
-document.addEventListener("keypress", function(event){
-  createSound(event.key);
-  buttonAnimation(event.key);
+  // Keyboard events for sounds
+  document.addEventListener("keypress", function(event) {
+    var key = event.key.toLowerCase();
+    createSound(key);
+    buttonAnimation(key);
+  });
 });
 
-function createSound(key){
+function createSound(key) {
   switch (key) {
     case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
@@ -28,11 +29,11 @@ function createSound(key){
     case "s":
       var tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
-      break; 
+      break;
     case "d":
       var tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
-      break;    
+      break;
     case "j":
       var snare = new Audio("sounds/snare.mp3");
       snare.play();
@@ -40,24 +41,21 @@ function createSound(key){
     case "k":
       var crash = new Audio("sounds/crash.mp3");
       crash.play();
-      break; 
+      break;
     case "l":
       var kick = new Audio("sounds/kick-bass.mp3");
       kick.play();
       break;
-                 
-    default: console.log(buttonInnerHtml);
-     
+    default:
+      console.log("Invalid key pressed");
   }
 }
 
-function buttonAnimation(currentKey){
-  var activeButton = document.querySelector("."+ currentKey);
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("area[data-sound='sounds/" + currentKey + ".mp3']");
   activeButton.classList.add("pressed");
-  
+
   setTimeout(() => {
-  activeButton.classList.remove("pressed");
+    activeButton.classList.remove("pressed");
   }, 100);
 }
-
-
